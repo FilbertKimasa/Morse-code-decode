de9461a7bdf6f9003857d1e2a943a601064bbace
FilbertKimasa <filbertdan67@gmail.com>
2023-11-16 18:38:38 +0300

    Fix linters erros

--- app.rb
+++ app.rb
@@ -1,25 +1,25 @@
 MORSE_CODE_DICT = {
-  'A'=>'.-', 'B'=>'-...',
-  'C'=>'-.-.', 'D'=>'-..', 'E'=>'.',
-  'F'=>'..-.', 'G'=>'--.', 'H'=>'....',
-  'I'=>'..', 'J'=>'.---', 'K'=>'-.-',
-  'L'=>'.-..', 'M'=>'--', 'N'=>'-.',
-  'O'=>'---', 'P'=>'.--.', 'Q'=>'--.-',
-  'R'=>'.-.', 'S'=>'...', 'T'=>'-',
-  'U'=>'..-', 'V'=>'...-', 'W'=>'.--',
-  'X'=>'-..-', 'Y'=>'-.--', 'Z'=>'--..',
-  '1'=>'.----', '2'=>'..---', '3'=>'...--',
-  '4'=>'....-', '5'=>'.....', '6'=>'-....',
-  '7'=>'--...', '8'=>'---..', '9'=>'----.',
-  '0'=>'-----', ' '=>'   '
-}
+  'A' => '.-', 'B' => '-...',
+  'C' => '-.-.', 'D' => '-..', 'E' => '.',
+  'F' => '..-.', 'G' => '--.', 'H' => '....',
+  'I' => '..', 'J' => '.---', 'K' => '-.-',
+  'L' => '.-..', 'M' => '--', 'N' => '-.',
+  'O' => '---', 'P' => '.--.', 'Q' => '--.-',
+  'R' => '.-.', 'S' => '...', 'T' => '-',
+  'U' => '..-', 'V' => '...-', 'W' => '.--',
+  'X' => '-..-', 'Y' => '-.--', 'Z' => '--..',
+  '1' => '.----', '2' => '..---', '3' => '...--',
+  '4' => '....-', '5' => '.....', '6' => '-....',
+  '7' => '--...', '8' => '---..', '9' => '----.',
+  '0' => '-----', ' ' => '   '
+}.freeze
 
 def decode_char(morse_char)
   MORSE_CODE_DICT.key(morse_char)
 end
 
 def decode_word(morse_word)
-  morse_chars = morse_word.split(' ')
+  morse_chars = morse_word.split
   decoded_word = ''
   morse_chars.each do |morse_char|
     char = decode_char(morse_char)
@@ -33,7 +33,7 @@ def decode_message(morse_message)
   decoded_message = ''
   morse_words.each do |morse_word|
     decoded_word = decode_word(morse_word)
-  decoded_message += decoded_word + ' '
+    decoded_message += "#{decoded_word} "
   end
   decoded_message.strip
 end
